21d6f20f27009ca489375faa8952e9bad044c9b1
Chaitanya Kulkarni <chaitanya.kulkarni@hgst.com>
2016-11-30 12:28:59 -0800

    block: add support for REQ_OP_WRITE_ZEROES
    This adds a new block layer operation to zero out a range of
    LBAs. This allows to implement zeroing for devices that don't use
    either discard with a predictable zero pattern or WRITE SAME of zeroes.
    The prominent example of that is NVMe with the Write Zeroes command,
    but in the future, this should also help with improving the way
    zeroing discards work. For this operation, suitable entry is exported in
    sysfs which indicate the number of maximum bytes allowed in one
    write zeroes operation by the device.
    
    Signed-off-by: Chaitanya Kulkarni <chaitanya.kulkarni@hgst.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit a6f0788ec2881ac14e97ff7fa6a78a807f87b5ba)
    Signed-off-by: Eduardo Valentin <eduval@amazon.com>
    Signed-off-by: Eduardo Valentin <edubezval@gmail.com>

--- block/blk-core.c
+++ block/blk-core.c
@@ -1925,6 +1925,10 @@ generic_make_request_checks(struct bio *bio)
 		if (!bdev_write_same(bio->bi_bdev))
 			goto not_supported;
 		break;
+	case REQ_OP_WRITE_ZEROES:
+		if (!bdev_write_zeroes_sectors(bio->bi_bdev))
+			goto not_supported;
+		break;
 	default:
 		break;
 	}
